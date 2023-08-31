9c2a217f2678180a79513a3a39f23da1ac46da05
akihiroozono <143259448+akihiroozono@users.noreply.github.com>
2023-08-31 10:47:38 +0900

    Update background.js

--- sample02/background.js
+++ sample02/background.js
@@ -1,11 +1,9 @@
-
 function createTab(){
     chrome.tabs.create({
         url: 'history.html',
         selected: true
     }, tab => {});
 }
-
 
 chrome.runtime.onInstalled.addListener(createTab);
 chrome.runtime.onStartup.addListener(createTab);
